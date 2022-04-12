bb3e4249a6500bb744abfbc0ac45cc0fe2d5be48
cola-mars <86696492+cola-mars@users.noreply.github.com>
2022-04-11 23:12:45 -0400

    cola/issue #1: initialize the react application

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+// import logo from './logo.svg';
+import Nav from './components/Nav';
+import About from './components/About';
+import './App.css';
+
+
+function App() {
+  return (
+    <div>
+      <Nav></Nav>
+      <main>
+        <About></About>
+      </main>
+
+    </div>
+  );
+}
+
+export default App;
