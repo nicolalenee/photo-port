d09a2f14004989b6c36429d62b8b3d67f8ee43d8
cola-mars <86696492+cola-mars@users.noreply.github.com>
2022-04-15 00:35:14 -0400

    issue#3: add function to handle form change and submit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Nav from './components/Nav';
 import About from './components/About';
 import './App.css';
 import Gallery from './components/Gallery';
+import ContactForm from './components/Contact';
 
 
 function App() {
@@ -37,6 +38,7 @@ function App() {
         currentCategory={currentCategory}
       ></Nav>
       <main>
+        <ContactForm></ContactForm>
         <Gallery currentCategory={currentCategory}></Gallery>
         <About></About>
       </main>
